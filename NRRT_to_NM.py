424124541cc87a1b78acea2f5f29c4f963ac8e6a
ioquirk <139350085+ioquirk@users.noreply.github.com>
2024-09-25 15:20:53 -0400

    Update NRRT_to_NM.py
    A modified version of the original script to make it easier use and for use outside of Blender by using the terminal to get image file path. I was constantly getting errors saying "IndexError: list index out of range".

--- NRRT_to_NM.py
+++ NRRT_to_NM.py
@@ -1,5 +1,6 @@
+# NRRT to NM Conversion Script - by TurboTimmy123 on GitHub
 # Converts MHR NRRT textures into OpenGL Normal maps with +45 degree rotation fix
-# Usage: python NRRT_to_NM.py [NRRT path]
+# Usage: Run Python file and insert image's file path in terminal.
 # Requires Python Imaging Library
 # pip install Pillow
 
@@ -12,7 +13,10 @@ def RotateVec45(x, y):
     qy = (R * x) + (R * y)
     return [qx, qy]
 
-name = sys.argv[1]
+name = input("Insert image file path: ")   # Automatically replaces '\' with '/' and removes '"'.
+name = name.replace('\\','/')
+name = name.replace('\"','')
+
 img = Image.open(name)
 newim = Image.new(mode="RGBA", size=(img.width, img.height))
 
@@ -20,9 +24,9 @@ for px in range(img.width):
     if px % 100 == 0: print(str(px) + "/" + str(img.width), end='\r', flush=True)
     for py in range(img.height):
         a = img.getpixel((px,py))
-        rot = RotateVec45(a[1]-127, (a[3]-127)) # NM data sits in Green/Alpha channels
-        newim.putpixel((px, py), (int(rot[1] + 127), int(rot[0] + 127), 255))
+        rot = RotateVec45(a[1]-127, (a[3]-127))     # (Y Normal, X Normal); Channels: R=[0], G=[1], B=[2], A=[3]
+        newim.putpixel((px, py), (int(rot[1] + 127), int(rot[0] + 127), 255))    # Values: (imageDimensions,R,G,B)
 
-newName = name[:name.rfind("_NRRT")] + "_NM.PNG"
-newim.save(newName, "PNG")
-print("Velkhana best monster")
+newName = name[:name.rfind("_NRRT")] + "_NM.png"
+newim.save(newName, "png")
+print(str(img.width) + "/" + str(img.height) + " ->> NRRT to NM Conversion done! Velkhana best monster\n")
